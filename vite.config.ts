d5b5455471ab4f782b319dd94b83877e30cd14ce
cls-martins <carlos.martins128@aluno.ce.gov.br>
2026-05-09 11:01:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite"; // <--- Adicione esta linha
 
-export default defineConfig();
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(), // O target costuma ser detectado pelo Nitro
+    nitro(),         // <--- Essencial para Vercel
+    viteReact(),
+  ],
+});
