159de7bc4dd8b1aa068237145288d46e15f393ae
cls-martins <carlos.martins128@aluno.ce.gov.br>
2026-05-09 14:37:27 -0300

    Delete app directory

--- app/client.tsx
+++
@@ -1,7 +0,0 @@
-import { createStartHandler } from '@tanstack/react-start/client'
-import { getRouter } from './router' // Ajuste o caminho para onde está seu arquivo de router
-
-const router = getRouter()
-const handler = createStartHandler({ router })
-
-handler()
